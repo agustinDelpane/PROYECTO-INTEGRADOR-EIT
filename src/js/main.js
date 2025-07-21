7c9a25fcea4aea95c86600c8f537da006fa95a74
Agustin Delpane <agustindelpane@gmail.com>
2025-07-21 16:54:30 -0300

    Estado final

---
+++ src/js/main.js
@@ -0,0 +1,188 @@
+const productList = [
+    ["Guitarra Eléctrica Ibanez Rg Gio Grx70qa Sunburst 6 Cuerdas", 10001,
+        `Caracterizada por su estilo, con cuello delgado y diapason más plano, electrónica de alta potencia y acabado colorido.
+        Esta marca es una de las más versátiles del mercado, ya que ofrece desde guitarras acústicas, eléctricas, bajos y amplificadores.
+        Tu próximo instrumento tiene que ser una Ibanez.`
+        , 270000],
+    ["Bajo Eléctrico Parquer Con Funda 4 Cuerdas", 20001,
+        `El Bajo Eléctrico Parquer Precision Negro y Blanco es el instrumento perfecto para los amantes de la música.
+        Con un acabado laqueado en negro y blanco, este bajo destaca por su elegancia y estilo. Fabricado con madera de tilo de alta
+        calidad, ofrece un sonido potente y definido. Su mástil de palo de rosa y sus 21 trastes permiten una ejecución cómoda y precisa.
+        Diseñado para diestros, este bajo es ideal para músicos de todos los niveles. Viene con una funda para
+        protegerlo y transportarlo fácilmente. Con cuatro cuerdas, este bajo ofrece una amplia gama de tonos y posibilidades musicales.
+        No pierdas la oportunidad de tener en tus manos este increíble bajo eléctrico Parquer Precision Negro y Blanco.`,
+        320000],
+    ["Guitarra Acústica Parquer GA105", 10002,
+        `Disfrutá con esta guitarra Parquer de la conexión con la música. Está diseñada para aficionados y profesionales. Con este
+        instrumento descubrirás nuevos acordes, entonarás tus canciones y disfrutarás de la vida musical.
+        La tapa de tilo tiene un peso y una densidad que te ofrecen un sonido con más cuerpo. Esta madera presenta un buen equilibrio de graves,
+        por lo que suena con más peso rítmico y un tono suave.
+        Las cuerdas de metal se caracterizan por su bajo estiramiento y resistencia a la corrosión y abrasión.
+        Son más duraderas, sólidas y generan un sonido brillante y claro. INCLUYE FUNDA.`,
+        145000],
+    ["Violín Acústico Segovia Superior 4/4", 30001,
+        `Descubre la elegancia y la calidad sonora del Violín Acústico Segovia Superior 4/4 Blanco Macizo Arco, una joya de
+        la marca Parquer que combina tradición y diseño contemporáneo. Este instrumento de tamaño completo 4/4 es ideal
+        tanto para estudiantes avanzados como para músicos profesionales que buscan un violín acústico con una estética refinada
+        y un sonido excepcional.`,
+        370000],
+    ["Cello Violoncello Segovia 3/4 Estudio Madera", 40001,
+        `Cello Violoncello Segovia 3/4 Estudio Madera Tilo Funda Arco' es un instrumento de alta calidad, perfecto para estudiantes y
+        músicos en desarrollo. Este violoncello de tamaño 3/4 está hecho de madera de tilo, conocida por su durabilidad y su capacidad
+        para producir un sonido cálido y resonante.
+        El instrumento viene con una funda protectora para facilitar su transporte y almacenamiento, así como un arco,
+        esencial para tocar el violoncello.`,
+        700000],
+    ["Ukelele Concierto Parquer Madera", 50001,
+        `Descubre el encantador Ukelele Concierto Parquer, una joya musical elaborada con madera de fresno de alta calidad, que
+        ofrece un sonido cálido y resonante. Este modelo UK-12BL, con un tamaño de 23 pulgadas, es perfecto tanto para principiantes
+        como para músicos experimentados. Su acabado mate no solo le proporciona un aspecto elegante, sino que también mejora la
+        experiencia de tocar. Las clavijas cromadas garantizan una afinación precisa y duradera, permitiéndote disfrutar de cada
+        acorde sin preocupaciones. Además, incluye una funda que facilita su transporte y protección, asegurando que tu instrumento
+        esté siempre a salvo.`,
+        115000],
+    ["Ukelele Soprano Parquer Madera", 50002,
+        `Descubre la armonía y la calidez del Ukelele Soprano Parquer, una obra maestra de la luthería que combina la belleza
+        estética con la calidad sonora. Este instrumento, con su tamaño de 21 pulgadas, es perfecto tanto para principiantes como
+        para músicos experimentados que buscan un sonido auténtico y una fácil portabilidad. La madera de Ovangkol flameado no solo
+        proporciona una resonancia rica y profunda, sino que también le da al ukelele un aspecto único con su color marrón oscuro y
+        acabado mate. Las clavijas cromadas aseguran una afinación estable y precisa, complementando su diseño elegante. Incluido con
+        el ukelele viene una funda a juego, que no solo protege tu instrumento, sino que también facilita su transporte a cualquier lugar
+        donde la música te lleve.`,
+        100000],
+    ["Bateria Acústica Parquer 5 Cuerpos Roja", 60001,
+        `Descubre la Batería Acústica Parquer de 5 cuerpos en un vibrante color rojo, perfecta para músicos adultos que buscan calidad
+        y estilo. Con un bombo de 22"x16", esta batería ofrece un sonido potente y resonante. Incluye un tom de pie de 16"x16", así como
+        toms de 12" y 10", garantizando una versatilidad excepcional en tu interpretación. El redoblante de 14"x5,5" añade un toque dinámico
+         a tus ritmos. Además, viene equipada con platos hihat de 14"x2 y un crash de 16"x1, junto con un pedal de bombo y un soporte
+         de pies dobles cromados para mayor estabilidad. Incluye baquetas y taburete.`,
+        800000],
+    ["Batería Electrónica Parquer Plegable 8 Pads Sensitiva Skd130", 60002,
+        `Bateria electronica plegable. Estable y de rapido ensamblaje. Configurable para shows o fiestas. Los nuevos Pad proveen una
+        performance natural. El trigger puede ser instalado con el martillo para proporcionar un efecto realista. El hihat ofrece una
+        voz natural. El cerebro posee abundantes voces. 250 voces de percusion. 20 baterias predefinidas + 10 baterias personalizables
+        por el usuario. 20 demos. Conexiones a Midi in / out / USB midi. aux in jack / line output / phone. Componentes: Modulo
+        Cerebro 7.5 " Snare x 1 con dos canales. rim/pad simultaneos 7.5 " toms x 3 mono 2" bass Hihat x 1 mono ride x 1 , crash x 1 Mono +
+        choke Hihat Pedal x 1. Recording and playback. Sensibilidad ajustable. curva de trigger y metronomo reverb. Cerebro con
+        transformador 9v / 500ma inside+, outside -.`,
+        760000],
+    ["Bateria Electronica Soundking Skd360 Sensitiva 8 Cuerpos", 60003,
+        `Incluye:
+            -Estructura / soporte
+            -3 Platillos: Hihat 10" x 1, Ride 10" x 1, Crash 10" x 1
+            -5 Cuerpos (MESH): Snare (Redo) 8" x 1, Toms 8 " x 3, Pad de Bombo x 1
+            -1 Modulo
+            -2 Pedales: Hihat x 1, Kick x 1 (+ pad de bombo)
+            -1 Par de palillos
+            -Transformador
+            -Manual
+
+        - Cuerpos de mesh (malla) con sensibilidad en el pad, y sonido en los aros.
+        - Platillos de goma sensitivos, que se silencian al agarrarlos con la mano.
+        - Modulo: Salida de auriculares (miniplug), Salida de audio stereo (plug L y R), Entrada auxiliar (Miniplug), Entrada de conexión para cuerpos, MIDI IN y OUT, Conexión USB, DC IN, 2 Entradas PLUG para cuerpos externos.
+        -Polifonia: 64
+        - Voces: 320 sonidos + 128 tonos de musica
+        - Kits de baterías: 25 presets + 12 de usuario
+        - Incluye metronomo y tempos variables
+        - Efectos.`,
+        1200000],
+    ["Bateria Tama Stagestar St52h6c-bns 5 Con Herrajes", 60004,
+        `TAMA tiene el equipo que necesitas, quieres y puedes permitirte sin importar cuánto tiempo lleves tocando
+        la batería o con qué presupuesto cuentes. Ahora, TAMA ha lanzado el equipo más asequible que jamás haya incorporado
+        muchas características interesantes: STAGESTAR. El nuevo equipo STAGESTAR viene con un soporte para tom Omni-ball y
+        herrajes con doble refuerzo para una mayor estabilidad.`,
+        1350800],
+    ["Teclado Parquer 61 Teclas Sensitivo Master Negro K186bk", 70001,
+        `Mejorá tus habilidades, explorá nuevos sonidos y dejá fluir tu creatividad. ¡Llená tu vida de música!
+        Pasá al siguiente nivel: Con sus 61 teclas, podrás tocar una amplia variedad de obras y sumergirte en el mundo del
+        intérprete musical. Ideal para niveles intermedios que quieren superarse y seguir el camino de los grandes músicos.`,
+        115000],
+    ["Interfaz Usb Audio Hugel Grabacion Profesional Stereo 2ch Color Negro", 80001,
+        `La interfaz USB Audio Hugel es la elección perfecta para aquellos que buscan una grabación profesional de alta calidad.
+         Con una resolución de audio de 24 bits, podrás capturar cada detalle y matiz de tus grabaciones. Su diseño en color
+         negro le da un aspecto elegante y moderno que se adapta a cualquier entorno de trabajo. Compatible con software
+         estándar, podrás utilizarlo con tus programas de grabación favoritos sin problemas. Con tres entradas y salidas,
+         tendrás la flexibilidad necesaria para conectar todos tus dispositivos de audio. Además, cuenta con alimentación
+         fantasma, lo que te permitirá utilizar micrófonos de condensador sin preocuparte por la energía. No pierdas la
+         oportunidad de mejorar tu experiencia de grabación con la interfaz USB Audio Hugel.`,
+        80000],
+]
+
+/*
+Cada tarjeta debe contener: imagen, título, código, descripción (usar ellipsis), precio, botón para agregar al carrito, 
+botón para quitar del carrito, contador de unidades agregadas (usar closure).
+*/
+
+const createCard = (product) => {
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+    card.innerHTML = `
+        <img class="card__image" src="./assets/images/products/${product[1]}.jpg" alt="Imagen de producto">
+        <h3 class="card__title">${product[0]}</h3>
+        <span class="card__code">${product[1]}</span>
+        <p class="card__description">${product[2]}</p>
+        <span class="card__price">${product[3].toLocaleString('es-AR')}</span>
+    `;
+
+    return card
+}
+
+const createCardWithCounter = (product) => {
+    let count = 0;
+
+    const card = createCard(product)
+
+    card.innerHTML += `
+    <div class="card__actions">
+            <button class="button button--add">Agregar al carrito</button>
+            <span class="card__counter">0</span>
+            <button class="button button--remove">Quitar del carrito</button>
+    </div>
+    `;
+
+    const addButton = card.querySelector('.button--add');
+    const removeButton = card.querySelector('.button--remove');
+    const counterSpan = card.querySelector('.card__counter');
+
+    addButton.addEventListener('click', () => {
+        count++;
+        counterSpan.textContent = count;
+    });
+
+    removeButton.addEventListener('click', () => {
+        if (count > 0) {
+            count--;
+            counterSpan.textContent = count;
+        }
+    });
+
+    return card;
+}
+
+function displayCards() {
+    const cardGallery = document.querySelector('.gallery');
+
+    productList.forEach(product => {
+        const card = createCardWithCounter(product);
+        cardGallery.appendChild(card);
+    });
+}
+
+function displayHighlitedCards(){
+    const highlightedProductsContainer = document.querySelector('.highlighted-products__container')
+    const highlightedProducts = [productList[0], productList[8], productList[11]]
+
+    highlightedProducts.forEach(product => {
+        const card = createCard(product);
+        highlightedProductsContainer.appendChild(card)
+    })
+}
+
+if (document.querySelector('.gallery')) {
+    displayCards();
+}
+
+if (document.querySelector('.highlighted-products__container')) {
+    displayHighlitedCards();
+}
